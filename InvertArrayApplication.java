27dd3e0b52d1287af8447a2487d5d11d5ed8c9ab
Tatiana <34233959+tanya00pm@users.noreply.github.com>
2017-12-04 14:44:39 +0300

    Add files via upload

---
+++ InvertArrayApplication.java
@@ -0,0 +1,30 @@
+package ru.levelup.tatiana_nesterova.qa.homework1.task3;
+
+public class InvertArrayApplication {
+    // Tatiana Nesterova, homework #1, task #3
+
+    public static void main(String[] args) {
+
+        InvertArrayApplication app = new InvertArrayApplication();
+        app.startApplication();
+    }
+
+    public void PrintArray(int a[]) {
+        // print array on console method
+
+        for (int i=0; i< a.length; i++)
+            System.out.println(Integer.toString(a[i]));
+    }
+
+    public void startApplication() {
+
+        InvertClass cl = new InvertClass(20, 55, 7);
+        System.out.println("Initial array:");
+        cl.PrintArray();
+
+        System.out.println("\nResult array:");
+
+        int mas[] =  cl.Invert();
+        this.PrintArray(mas);
+    }
+}
---
+++ InvertClass.java
@@ -0,0 +1,45 @@
+package ru.levelup.tatiana_nesterova.qa.homework1.task3;
+import java.util.Random;
+
+public class InvertClass {
+    private int arr[];
+
+    public InvertClass(int[] arr) {
+        // constructor
+        this.arr = arr;
+    }
+
+    public InvertClass(int random_min, int random_max, int array_size) {
+        // constructor for array with random numbers
+
+        arr = new int[array_size];
+        Random rand = new Random();
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i]  = random_min + (int) (Math.random() * random_max);
+
+    }
+
+    public void PrintArray() {
+        // print current array method
+
+        for (int i=0; i< arr.length; i++)
+            System.out.println(Double.toString(arr[i]));
+    }
+
+    public int[] Invert() {
+        // invert array method
+
+        int[] result_arr = new int[arr.length];
+        double temp;
+
+        int j = 0;
+        for (int i = arr.length-1; i >= 0; i--)
+        {
+            result_arr[j]=arr[i];
+            j++;
+        }
+        return result_arr;
+    }
+
+}
